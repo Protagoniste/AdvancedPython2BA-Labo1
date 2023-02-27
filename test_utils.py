26bb12cd3b7116e018fe37168c5d4145ba10846d
Abdelbadi <20369@ecam.be>
2023-02-27 08:54:50 +0100

    initial commit

--- test_utils.py
+++ test_utils.py
@@ -1,1 +1,15 @@
-projet informatique
+import pytest
+import utils
+
+def test_fact():
+    # À compléter...
+    pass
+
+def test_roots():
+    # À compléter...
+    pass
+
+def test_integrate():
+    # À compléter...
+    pass
+
--- utils.py
+++ utils.py
@@ -1,1 +1,14 @@
-projet informatique
+import pytest
+import utils
+
+def test_fact():
+    # À compléter...
+    pass
+
+def test_roots():
+    # À compléter...
+    pass
+
+def test_integrate():
+    # À compléter...
+    pass
