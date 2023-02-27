f1c2cd73ec87d98cb388fbb378325f26b168562d
Abdelbadi <20369@ecam.be>
2023-02-27 11:45:11 +0100

    commit

--- utils.py
+++ utils.py
@@ -1,23 +1,49 @@
+import math
 def fact(n):
-	"""Computes the factorial of a natural number.
+    
+    """Computes the factorial of a natural number.
 	
 	Pre: -
 	Post: Returns the factorial of 'n'.
 	Throws: ValueError if n < 0
 	"""
-	pass
 
+    
+    if n == 0 :
+        return 1
+        
+    if n < 0 :
+        raise ValueError("n must be positive")
+
+    
+
+import math
 def roots(a, b, c):
-	"""Computes the roots of the ax^2 + bx + x = 0 polynomial.
+
+    """Computes the roots of the ax^2 + bx + x = 0 polynomial.
 	
 	Pre: -
 	Post: Returns a tuple with zero, one or two elements corresponding
 		to the roots of the ax^2 + bx + c polynomial.
 	"""
-	pass
 
+    delta = b**2 - 4*a*c
+    
+    if delta < 0 :
+        return tuple()
+    
+    elif delta == 0 :
+        return (-b/2*a,)
+    
+    else :
+        return ((-b+ math.sqrt(delta))/2*a,(-b- math.sqrt(delta))/2*a)
+    
+    
+        
+
+from scipy import integrate as itg
 def integrate(function, lower, upper):
-	"""Approximates the integral of a fonction between two bounds
+    """Approximates the integral of a fonction between two bounds
 	
 	Pre: 'function' is a valid Python expression with x as a variable,
 		'lower' <= 'upper',
@@ -29,7 +55,13 @@ def integrate(function, lower, upper):
 		you'll probably need the 'eval' function to evaluate the function
 		to integrate given as a string.
 	"""
-	pass
+
+
+    integrand = lambda x: eval(function)
+    result = itg.quad(integrand, lower, upper)
+    return result
+
+
 
 if __name__ == '__main__':
 	print(fact(5))
