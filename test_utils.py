b10fc4a479189a71df8e0c012f10128d069c0a5e
Abdelbadi <20369@ecam.be>
2023-03-07 12:50:11 +0100

    commit

--- test_utils.py
+++ test_utils.py
@@ -4,11 +4,13 @@ import utils
 
 def test_fact():
         assert utils.math.factorial(5) == 120
+        assert utils.math.factorial(0) == 1
+        with pytest.raises(ValueError):
+            utils.fact(-1)
 
 '''    assert utils.fact(0) == 1
     assert utils.fact(4) == 24
-    with pytest.raises(ValueError):
-        utils.fact(-1) '''
+     '''
 
 def test_roots():
     pass
